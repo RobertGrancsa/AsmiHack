fbab28798bfbc641650a10678262f3aaad8677e8
RaresCon <rares.constantin2002@gmail.com>
2022-11-05 21:49:07 +0200

    am un creier AMG, depasesc problemeeele

--- app/src/main/java/com/redrevorebourne/asmihack/MainActivity.java
+++ app/src/main/java/com/redrevorebourne/asmihack/MainActivity.java
@@ -5,6 +5,7 @@ import static android.content.ContentValues.TAG;
 import androidx.annotation.NonNull;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -18,10 +19,13 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.util.Locale;
+
 public class MainActivity extends Activity {
     private FirebaseAuth mAuth;
     private TextView emailbox;
-    private Button loginButton;
+    private TextView passwordbox;
+    private static int counter = 0;
 
     private Button button;
 
@@ -34,14 +38,13 @@ public class MainActivity extends Activity {
         String password = "emilsrevange";
 
         emailbox = findViewById(R.id.emailBox);
+        passwordbox = findViewById(R.id.passwordBox);
 
-        loginButton = findViewById(R.id.buttonConfirm);
+        Button loginButton = findViewById(R.id.login);
 
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
+        loginButton.setOnClickListener(view -> {
+            Log.d(TAG, "onClick: " + emailbox.getText() + passwordbox.getText());
+            signIn(emailbox.getText().toString(), passwordbox.getText().toString());
         });
 
         // Initialize Firebase Auth
@@ -57,39 +60,46 @@ public class MainActivity extends Activity {
         if(currentUser != null){
             reload();
         }
+
+        counter++;
     }
 
     private void reload() { }
 
     private void createAccount(String email, String password) {
-        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "createUserWithEmail:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            updateUI(user);
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                            Toast.makeText(MainActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                            updateUI(null);
-                        }
-                    }
-                });
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, task -> {
+            if (task.isSuccessful()) {
+                // Sign in success, update UI with the signed-in user's information
+                Log.d(TAG, "createUserWithEmail:success");
+                FirebaseUser user = mAuth.getCurrentUser();
+                updateUI(user);
+            } else {
+                // If sign in fails, display a message to the user.
+                Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                Toast.makeText(MainActivity.this, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show();
+                updateUI(null);
+            }
+        });
     }
 
-    private void updateUI(FirebaseUser user) { }
+    private void updateUI(FirebaseUser user) {
+        if (user != null) {
+            Intent intent = new Intent(MainActivity.this, StartScreenActivity.class);
 
+            startActivity(intent);
+            finish();
+        }
+    }
 
     private void signIn(String email, String password) {
         // [START sign_in_with_email]
-        mAuth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
+        if ((email.equals("") || password.equals("")) && counter == 1) {
+            Toast.makeText(MainActivity.this, "Authentication failed.",
+                    Toast.LENGTH_SHORT).show();
+        } else {
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, task -> {
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
                             Log.d(TAG, "signInWithEmail:success");
@@ -102,8 +112,8 @@ public class MainActivity extends Activity {
                                     Toast.LENGTH_SHORT).show();
                             updateUI(null);
                         }
-                    }
-                });
+                    });
+        }
         // [END sign_in_with_email]
     }
 }
