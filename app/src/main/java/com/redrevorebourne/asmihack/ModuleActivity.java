1fd6c089fd76f87cf6f62b843d770e465e62b619
Pandi-chan30 <andreea.dre02@gmail.com>
2022-11-06 02:51:34 +0200

    Nu stiu care-i faza cu url img si altele asemenea, but here we go

--- app/src/main/java/com/redrevorebourne/asmihack/ModuleActivity.java
+++ app/src/main/java/com/redrevorebourne/asmihack/ModuleActivity.java
@@ -1,4 +1,49 @@
 package com.redrevorebourne.asmihack;
 
-public class ModuleActivity {
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+
+public class ModuleActivity extends Activity {
+    private TextView moduleName;
+    private ImageView moduleImage;
+    private TextView moduleRate;
+    private TextView moduleNumberDownloads;
+    private TextView moduleDescription;
+    private TextView moduleContact;
+    private TextView moduleURL;
+
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_module);
+
+        moduleName = findViewById(R.id.moduleName);
+        moduleName.setText(getIntent().getStringExtra("name"));
+
+        //imageModule = findViewById(R.id.imageModules);
+        //imageModule = set
+
+        moduleRate = findViewById(R.id.rateBox);
+        moduleRate.setText(getIntent().getStringExtra("rating"));
+
+        moduleNumberDownloads = findViewById(R.id.moduleDownloads);
+        moduleNumberDownloads.setText(getIntent().getStringExtra("downloads"));
+
+        moduleDescription = findViewById(R.id.moduleDescription);
+        moduleDescription.setText(getIntent().getStringExtra("description"));
+
+        moduleContact = findViewById(R.id.moduleContact);
+        moduleContact.setText(getIntent().getStringExtra("description"));
+
+        moduleURL = findViewById(R.id.moduleURL);
+        moduleURL.setText(getIntent().getStringExtra("url"));
+    }
 }
